57c4c76df992f4c75e6136034e0ce0e56ac7885d
yacine elgoub <yacinoelgo@gmail.com>
2017-12-11 21:52:18 +0000

    Update TypefaceUtil.java

--- TypefaceUtil.java
+++ TypefaceUtil.java
@@ -21,7 +21,7 @@ public class TypefaceUtil {
             defaultFontTypefaceField.setAccessible(true);
             defaultFontTypefaceField.set(null, customFontTypeface);
         } catch (Exception e) {
-            Log.e("Can not set custom font " + customFontFileNameInAssets + " instead of " + defaultFontNameToOverride);
+            
         }
     }
 }
